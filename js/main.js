2abdf03779117e0af4f58b79cdb3e378414f52a4
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-08-21 17:52:30 +0200

    index, style and js: edit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const button = document.getElementById("generates");
+const button = document.getElementById("generate");
 const grid = document.getElementById("grid");
 
 button.addEventListener("click", function () {
@@ -6,16 +6,21 @@ button.addEventListener("click", function () {
 });
 
 function generateGrid() {
-  for (let index = 1; index <= 100; index++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.innerHTML = index;
-
+  for (let i = 1; i <= 100; i++) {
+    const cell = generateCell(i);
     grid.append(cell);
-
-    cell.addEventListener("click", function () {
-      cell.classList.add(azure);
-      console.log(index);
-    });
   }
+}
+
+function generateCell(i) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.innerHTML = i;
+
+  cell.addEventListener("click", function () {
+    cell.classList.add("azure");
+    console.log(i);
+  });
+
+  return cell;
 }
