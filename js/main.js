2625f06a07497a438f7583299ad932a451d2461a
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-08-21 16:34:10 +0200

    index, style and js: add grid

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-const button = document.getElementById(generates);
+const button = document.getElementById("generates");
+const grid = document.getElementById("grid");
 
 button.addEventListener("click", function () {
   generateGrid();
 });
+
+function generateGrid() {
+  for (let index = 1; index <= 100; index++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.innerHTML = index;
+
+    grid.append(cell);
+
+    cell.addEventListener("click", function () {
+      cell.classList.add(azure);
+      console.log(index);
+    });
+  }
+}
